020fb71c55ce565132ef2cf2425d165a60d3cf2c
sj <dlstjdwl@gmail.com>
2023-01-28 14:05:45 +0900

    맥에서 커밋 3장 주석메모 및 퀴즈 풀이(뷰 컴포넌트간 이벤트와 속성 알아보기)

--- doit-vuejs/quiz/03-2/sample/js/app.js
+++ doit-vuejs/quiz/03-2/sample/js/app.js
@@ -1,6 +1,15 @@
 // 실습 #1 - `todo-footer` 컴포넌트 전역 등록
 // <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
 
+
+var cmp = {
+  template: '<p>This is another local child component</p>'
+}
+
+Vue.component ('todo-footer',{
+   template: '<p>This is another global child component</p>'
+})
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -9,4 +18,7 @@ var app = new Vue({
 
   // 실습 #2 - `todo-list` 컴포넌트 지역 등록
   // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+  component : {
+    'todo-list' : cmp
+  }
 });
